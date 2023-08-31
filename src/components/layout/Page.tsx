d8073c6944db7e530b2ceb8560b1d21a6cb57381
Richard Chapman <rcchapman85@gmail.com>
2023-08-31 20:57:49 +0100

    refactor: optional page title

--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -1,16 +1,18 @@
 type PageProps = {
-  title: string;
+  title?: string;
   children: React.ReactNode;
 };
 
 export const Page = ({ title, children }: PageProps) => {
   return (
     <>
-      <header>
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-6">
-          <h1 className="text-3xl font-medium text-gray-900">{title}</h1>
-        </div>
-      </header>
+      {title && (
+        <header>
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-6">
+            <h1 className="text-3xl font-medium text-gray-900">{title}</h1>
+          </div>
+        </header>
+      )}
       <main>
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 pb-10">
           {children}
