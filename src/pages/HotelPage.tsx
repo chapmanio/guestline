b8bea205ba53933ebcf159bfcbda2d49cd21a658
Richard Chapman <rcchapman85@gmail.com>
2023-09-01 15:17:01 +0100

    feat: hotel page error handling

--- src/pages/HotelPage.tsx
+++ src/pages/HotelPage.tsx
@@ -2,19 +2,19 @@ import { useParams } from "react-router";
 import { Page } from "../components/layout";
 import { useHotel } from "../hooks/useHotel";
 import { HotelDetails, HotelDetailsSkeleton } from "../components";
+import { ErrorPage } from ".";
 
 export const HotelPage = () => {
   const { id } = useParams<"id">();
 
-  const { data, isLoading } = useHotel(id);
+  const { data, isLoading, error } = useHotel(id);
 
-  return (
+  return !error ? (
     <Page>
-      {isLoading ? (
-        <HotelDetailsSkeleton />
-      ) : (
-        <>{data && <HotelDetails hotel={data} />}</>
-      )}
+      {isLoading && <HotelDetailsSkeleton />}
+      {data && <HotelDetails hotel={data} />}
     </Page>
+  ) : (
+    <ErrorPage />
   );
 };
