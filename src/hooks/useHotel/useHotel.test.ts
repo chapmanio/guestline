0c097b04095a68088801a509206dda74913dfbb9
Richard Chapman <rcchapman85@gmail.com>
2023-09-01 20:15:45 +0100

    test: useHotel tests

---
+++ src/hooks/useHotel/useHotel.test.ts
@@ -0,0 +1,39 @@
+import { describe, it, expect } from "vitest";
+import { renderHook, waitFor } from "@testing-library/react";
+import { mockedHotels, mockedRoomsAndRates, wrapper } from "../../tests";
+import { useHotel } from ".";
+
+describe("useHotels", () => {
+  it("retrieves a hotel based on supplied id", async () => {
+    const hotelId = "fake-hotel-1";
+
+    const { result } = renderHook(() => useHotel(hotelId), { wrapper });
+
+    await waitFor(() => expect(result.current.isLoading).toBe(false));
+
+    expect(result.current.data).toEqual({
+      ...mockedHotels[0],
+      ...mockedRoomsAndRates,
+    });
+  });
+
+  it("errors with no supplied id", async () => {
+    const { result } = renderHook(useHotel, { wrapper });
+
+    await waitFor(() =>
+      expect(result.current.failureReason).toEqual(
+        Error("A hotel ID must be supplied"),
+      ),
+    );
+  });
+
+  it("errors with an unmatched id", async () => {
+    const { result } = renderHook(() => useHotel("unknown"), { wrapper });
+
+    await waitFor(() =>
+      expect(result.current.failureReason).toEqual(
+        Error("No hotel found with id: unknown"),
+      ),
+    );
+  });
+});
