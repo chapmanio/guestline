a5e5fb0874ddb398b034ec603e17fd7e21d44de9
Richard Chapman <rcchapman85@gmail.com>
2023-09-01 21:51:02 +0100

    test: add testing library matchers

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,7 +1,15 @@
 import "whatwg-fetch";
-import { afterEach, afterAll, beforeAll } from "vitest";
+import { afterEach, afterAll, beforeAll, expect } from "vitest";
+import { cleanup } from "@testing-library/react";
+import * as matchers from "@testing-library/jest-dom/matchers";
+
 import { server } from "./mocks";
+
+expect.extend(matchers);
 
 beforeAll(() => server.listen({ onUnhandledRequest: `error` }));
 afterAll(() => server.close());
-afterEach(() => server.resetHandlers());
+afterEach(() => {
+  server.resetHandlers();
+  cleanup();
+});
